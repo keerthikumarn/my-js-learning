d951afa5df2dc2091f16b43c7d4c3a8c36f14d55
Keerthi Kumar N <keerthi.narayan@subex.com>
2024-02-08 12:36:39 +0530

    objects related

---
+++ 03-Intermediate/10Objects.js
@@ -0,0 +1,10 @@
+var employee = {
+    firstName: "Keerthi",
+    lastName: "Kumar",
+    designation: "Technical Architect",
+    place: "Bangalore"
+}
+
+console.log(employee.firstName);
+console.log(employee["lastName"]);
+console.table(employee);
