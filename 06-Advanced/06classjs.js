e9555a03fabd8d8f295f12411b88d9929edc1c11
Keerthi Kumar N <keerthi.narayan@subex.com>
2024-03-12 23:08:35 +0530

    updated with private variable

--- 06-Advanced/06classjs.js
+++ 06-Advanced/06classjs.js
@@ -5,20 +5,27 @@ class User {
         this.email = email;
     }
 
-    courseList = [];
+    #courseList = [];
 
     getInfo() {
         return {name: this.name, email: this.email}
     }
 
     enrollCourse(name) {
-        this.courseList.push(name);
+        this.#courseList.push(name);
     }
 
     getCourseList() {
-        return this.courseList;
+        return this.#courseList;
     }
 }
 
 
 module.exports = User;
+
+const keerthi = new User("Keerthi Kumar N", "keerthi@no-reply.com");
+console.log(keerthi.getInfo());
+keerthi.enrollCourse("Advanced Java");
+keerthi.enrollCourse("Springboot");
+console.log(keerthi.getCourseList());
+console.log(keerthi.courseList);
